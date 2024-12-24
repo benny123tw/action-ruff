376584cb59a09807de3db7d1dab1fb6162256163
Benny Yen <benny123tw@gmail.com>
2024-12-24 22:11:55 +0800

    ci: add test workflow

---
+++ testdata/test.py
@@ -0,0 +1,8 @@
+# `typing.List` is deprecated, use `list` instead
+from typing import List
+
+
+# Use `list` instead of `List` for type annotation
+def test(builtin: List[str]):
+  ...
+# No newline at end of file
